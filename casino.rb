78ca79c7fc851fb147af52dead17148470ec6663
Jaxon Porter <jaxonutah@gmail.com>
2019-02-15 23:01:24 -0700

    finished casino class and highlow game.

--- casino.rb
+++ casino.rb
@@ -6,98 +6,66 @@ require "pry"
 
 class Casino
   attr_accessor :menuoptions
-  def initialize
-    @menuoptions = ["Blackjack", "Roulette", "Slots", "High-low"]
-
+  def initialize(name, wallet)
+    @menuoptions = ["Blackjack", "Roulette", "Slots", "Lucky-Dice", "View Balance", "Quit"]
+    @userwallet = wallet
+    @username = name
     menu
-
+    selection
   end
 
   def menu
+    puts "======================================"
+    puts "      Welcome to the Casino."
+    puts "   What would you like to play?\n "
+    @menuoptions.each_with_index do |game, index|
+      puts "#{index+1}) #{game}"
+    end
+  end
+    
+  def selection
+    puts
+    print "> "
+    choice = gets.strip.to_i
+    case choice
+      when 1
+        Blackjack.new(@username, @userwallet)
+      when 2
+        Roulette.new(@username, @userwallet)
+      when 3
+        Slots.new(@username, @userwallet)
+      when 4
+        Luckydice.new(@username, @userwallet)
+      when 5
+        puts "\nYou have $#{@userwallet}"
+      when 6
+        cash_out
+      else
+        puts "\nInvalid entry, try again."
+        menu
+    end
+  end
+  def cash_out
+    puts "Thank you for coming. Your balance is $#{@userwallet}."
+    exit
   end
     
 end
+    
 
-class Userinfo
-  attr_accessor :wallet, :name
-
-  def initialize
-    puts "\nHello, welcome to our Casino, what is your name?"
+def start_game
+  puts "\nHello, before you enter the Casino, what is your name?"
+  print "> "
+  name = gets.strip
+  puts "\nHow much money do you have?"
+  print "> $"
+  wallet = gets.strip.to_i
+  while wallet < 2
+    puts "Invalid Entry, try again"
     print "> "
-    @name = gets.strip
-    puts "\nHow much money do you have?"
-    print "> $"
-    @wallet = gets.strip.to_f
-    while @wallet < 2
-      puts "Invalid Entry, try again"
-      print "> "
-      @wallet = gets.strip.to_f
-    end
+    wallet = gets.strip.to_i
   end
+  Casino.new(name, wallet)
 end
-
-end
-class Card
-  # Getter and Setter methods for rank, suit and color
-  attr_accessor :rank, :suit, :color
-  # Gets called when you call the new method to create an instance
-  # card = Card.new('10', 'K', 'Black')
-  def initialize(rank, suit, color)
-    @rank = rank
-    @suit = suit
-    @color = color
-  end
- end
-
- class Deck
-  # Getter and Setter methods for rank, suit and color
-  attr_accessor :cards
  
-  # Gets called when you call the new method to create an instance
-  # deck = Deck.new
-  def initialize
-    @ranks = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
-    @suits = %w(Spades Diamonds Clubs Hearts)
-    @cards = []
-    generate_deck
-  end
- 
-  def shuffle_cards
-   @cards.shuffle
-  end
-  
-  def generate_deck
-    @suits.each do |suit|
-      @ranks.size.times do |i|
-        # Ternary Operator
-        color = (suit == 'Spades' || suit == 'Clubs') ? 'Black' : 'Red' 
-        @cards << Card.new(@ranks[i], suit, color)
-      end
-    end
-  end
- 
-   def display_cards
-     @cards.shuffle.each do |card|
-       puts "#{card.rank} #{card.suit} (#{card.color})"
-     end
-   end
- end
-
-class Dice
-  def initialize
-    roll
-  end
-  
-  def roll
-    @die1 = 1 + rand(6)
-    @die2 = 1 + rand(6) 
-  end
-  
-  def show_dice
-    print "Die1: ", @die1, " Die2:", @die2
-  end
-  
-  def show_sum
-    print "Sum of dice is ", @die1 + @die2, ".\n"
-  end 
- end
+start_game
