71d28857e6437c3f7ce69abc2f04c9d8ad5f9e84
Micah Magneson <mpmss1@yahoo.com>
2019-02-17 14:10:55 -0700

    slots.rb

--- slots.rb
+++ slots.rb
@@ -1,12 +1,12 @@
-require_relative "casino.rb"
+# require_relative Casino
 require "colorize"
 class Slots
   def initialize
-    wallet = 5000
-    @wallet = wallet
-    run_slots
+      wallet = 5000
+      @wallet = wallet
+      run_slots
   end
-  
+ 
 
   def multiplier(s1, s2, s3)
     if s1==s2 && s2==s3
@@ -20,8 +20,11 @@ class Slots
 
   def run_slots
     slotImageList = %w[Cherry Orange Plum Bell Melon Bar]
-    puts "You have $#{@wallet}".green
+
+    puts "How much total money would you like to play with today? ".yellow
+    @wallet = gets.chomp.to_i
     loop do
+      puts "Total cash:  $#{@wallet}".green
       puts "How much would you like to bet? ".yellow
       bet = gets.chomp.to_i
 
@@ -31,20 +34,17 @@ class Slots
       slotImage2 = slotImageList.sample
       slotImage3 = slotImageList.sample
 
-      puts "#{slotImage1} - #{slotImage2} - #{slotImage3}".cyan
+      puts "#{slotImage1} - #{slotImage2} - #{slotImage3}".red
 
       winnings = bet * multiplier(slotImage1, slotImage2, slotImage3)
-      puts "You have won $#{winnings}".green
+      puts "You have won $#{winnings}".cyan
 
       @wallet += winnings
 
       puts "Would you like to continue? (yes to continue) ".yellow
-      unless "y"
-        run_slots
-        # # Casino
-      else 
-        exit
-      break
+      unless gets.chomp=="yes"
+        puts "You have ended with $#{@wallet}".green
+        break
       end
     end
   end
